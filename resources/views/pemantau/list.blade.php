a999d89fd0cbbda423bdc10731889145070d8688
Nama Anda <email@anda.com>
2025-08-12 13:46:34 +0700

    edit user

--- resources/views/pemantau/list.blade.php
+++ resources/views/pemantau/list.blade.php
@@ -12,11 +12,12 @@
                     <tr>
                         <th>QR Code</th>
                         <th>Nama Anak</th>
-                        <th>Nama Orang Tua</th>
+                        <th>Nama Aya</th>
+                        <th>Nama Ibu</th>
                         <th>Sekolah</th>
                         <th>Kelas</th>
                         <th>Status</th>
-                        <th>Aksi</th>
+
                     </tr>
                 </thead>
                 <tbody>
@@ -26,7 +27,8 @@
                                 <span class="badge bg-primary">{{ $recipient->qr_code }}</span>
                             </td>
                             <td>{{ $recipient->child_name }}</td>
-                            <td>{{ $recipient->parent_name }}</td>
+                            <td>{{ $recipient->Ayah_name }}</td>
+                            <td>{{ $recipient->Ibu_name }}</td>
                             <td>{{ $recipient->school_name }}</td>
                             <td>{{ $recipient->class }}</td>
                             <td>
@@ -36,32 +38,7 @@
                                     <span class="badge bg-warning">Belum Menerima</span>
                                 @endif
                             </td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a href="{{ route('recipients.show', $recipient) }}" class="btn btn-sm btn-info">
-                                        <i class="fas fa-eye"></i>
-                                    </a>
-                                    <a href="{{ route('recipients.edit', $recipient) }}" class="btn btn-sm btn-warning">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a href="{{ route('recipients.qr-code', $recipient) }}" class="btn btn-sm btn-secondary" target="_blank">
-                                        <i class="fas fa-qrcode"></i>
-                                    </a>
-                                    @if($recipient->is_distributed)
-                                        <a href="{{ route('recipients.receipt', $recipient) }}" class="btn btn-sm btn-success">
-                                            <i class="fas fa-file-pdf"></i>
-                                        </a>
-                                    @endif
-                                    <form action="{{ route('recipients.destroy', $recipient) }}" method="POST" class="d-inline"
-                                          onsubmit="return confirm('Yakin ingin menghapus data ini?')">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger">
-                                            <i class="fas fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
+
                         </tr>
                     @empty
                         <tr>
